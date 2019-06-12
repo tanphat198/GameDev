ce82f240d12b3444723eb82e65a70e39857c627c
tanphat198 <44112014+tanphat198@users.noreply.github.com>
2019-06-12 23:06:57 +0700

    tao vừa làm xong A03
    xoắn vcl

---
+++ A03/Assets/Scenes/spidermancontroll.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spidermancontroll : MonoBehaviour {
+
+
+    Vector2 newPos;
+    public float speed = 1.0f;
+	// Use this for initialization
+	void Start () {
+        newPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-speed, 0));
+            GetComponent<SpriteRenderer>().flipX = true;
+            //newPos.x -= speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(+speed, 0));
+            //newPos.x += speed;
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0 , +speed*5));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -speed));
+        }
+    }
+}
