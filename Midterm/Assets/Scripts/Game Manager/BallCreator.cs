19c7e08af2813087174581bea0364988e244a4fe
tanphat198 <44112014+tanphat198@users.noreply.github.com>
2019-07-16 21:47:47 +0700

    Update

---
+++ Midterm/Assets/Scripts/Game Manager/BallCreator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallCreator : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject ball;
+    private GameObject wood;
+
+	[SerializeField]
+	private Sprite[] ballImages;
+    private Sprite woodImages;
+
+	private float minX = -4.7f, maxX = 6.5f, minY = -2.5f, maxY = 1.5f;
+
+	public void CreateBall(int index) {
+		GameObject gameBall = Instantiate(ball, new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0),
+		                                  Quaternion.identity) as GameObject;
+
+		gameBall.GetComponent<SpriteRenderer> ().sprite = ballImages [index];
+	}
+
+    public void CreateRandomObject()
+    {
+        GameObject randomWood = Instantiate(wood, new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0),
+                                          Quaternion.identity) as GameObject;
+
+        randomWood.GetComponent<SpriteRenderer>().sprite = woodImages;
+    }
+
+
+} // BallCreator
